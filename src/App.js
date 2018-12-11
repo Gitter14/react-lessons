eedfad1699817335802fe2661257ef3b10532145
Maciek <ubernam@gmail.com>
2018-12-11 18:13:14 +0100

    Test code removed: 'ErrorBoundary' and throw new error('...')

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -22,8 +21,7 @@ class App extends Component {
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(person => {
-      //return person.id === id;
-      return person.userId === id;
+      return person.id === id;
     });
     const person = {
       ...this.state.persons[personIndex]
@@ -51,9 +49,8 @@ class App extends Component {
       person = (
         <div>
           {this.state.persons.map((person,index) => {
-            return <ErrorBoundary
-              key={person.id}>
-              <Person
+            return <Person
+              key={person.id}
             // the same as below:
             //click={() => this.deletePersonsHandler(index)}
             click={this.deletePersonsHandler.bind(this,index)}
@@ -62,7 +59,6 @@ class App extends Component {
             name={person.name} 
             age={person.age}
             />
-            </ErrorBoundary>
           })}
         </div>
       );
