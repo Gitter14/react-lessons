9441ddf5a9df9291f362d8c7adef172fab367e16
Maciek <ubernam@gmail.com>
2018-12-28 20:02:03 +0100

    Changed Cockpit.js render div wrapper so that btnClass handles button styling, and src/hoc/Auxilery.js function added that can wrap cockpit elements in itself

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './Cockpit.css'
+import Aux from '../../hoc/Auxilery';
 
 const cockpit = (props) => {
 
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
 
     if(props.showPersons)
-        btnClass = classes.Red
+        btnClass = [classes.Button, classes.Red].join(' ');
 
     if(props.persons.length <= 2){
       assignedClasses.push(classes.red);
@@ -18,7 +19,7 @@ const cockpit = (props) => {
     }
 
     return (
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!!!</p>
             <button
@@ -26,7 +27,7 @@ const cockpit = (props) => {
             onClick={props.clicked}>
             Show/Hide persons
             </button>
-        </div>
+        </Aux>
     );
 }
 
